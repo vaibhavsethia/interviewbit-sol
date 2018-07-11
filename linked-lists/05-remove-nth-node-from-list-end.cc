7bc3a5c27be131d0e947a5054a218d85dd940425
Deepak Aggarwal <daggarwal77@gmail.com>
2018-07-11 11:24:05 +0530

    05-remove-nth-node-from-list-end.cc

---
+++ linked-lists/05-remove-nth-node-from-list-end.cc
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::removeNthFromEnd(ListNode* A, int B) {
+    ListNode* nAhead= A;
+    
+    for(int curNodeCount = 0; nAhead && curNodeCount < B; ++curNodeCount){
+        nAhead = nAhead->next;
+    }
+        
+    ListNode* cur = A;
+    ListNode* prev = NULL;
+    while(cur && nAhead){
+        prev = cur;
+        cur = cur->next;
+        nAhead = nAhead->next;
+    }
+    
+    if (prev){
+        prev->next = cur->next;
+        delete cur;
+    }
+    else {
+        A = cur->next;
+        delete cur;
+    }
+    return A;
+    
+}
