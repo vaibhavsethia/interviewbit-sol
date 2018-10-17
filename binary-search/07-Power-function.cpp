3a2522846ddec790cdf83dfc2190660f6860d34d
vaibhavsethia <40268761+vaibhavsethia@users.noreply.github.com>
2018-10-17 18:55:06 +0530

    Create 07-Power-function.cpp

---
+++ binary-search/07-Power-function.cpp
@@ -0,0 +1,28 @@
+// https://www.interviewbit.com/problems/implement-power-function/
+
+int Solution::pow(int x, int n, int d) {
+    long long int rem = 1;
+    int check = 0;
+    if(x == 0)
+        return 0;
+    if(n == 0)
+        return 1;
+    if(x < 0){
+        x = abs(x);
+        if(n%2 != 0)
+            check = 1;
+    }
+    long long int temp = x%d;
+    while(n != 0){
+        if(n%2 != 0)
+            rem = (rem*temp)%d;
+        temp = temp*temp;
+        temp = temp%d;
+        n = n/2;
+        if(rem > d)
+            rem = rem%d;
+    }
+    if(check == 1)
+        return d-(int)rem;
+    return (int)rem;
+}
